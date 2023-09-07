06e734df79c0993729423f7d1975bb006f145a4e
Alex Liebhaber <alexliebhaber1992@gmail.com>
2023-09-07 18:33:51 -0500

    starting traversy ds & algos

---
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -0,0 +1,9 @@
+function calculator(a, b, op) {
+  if (op === "+") return a + b;
+  if (op === "-") return a - b;
+  if (op === "*") return a * b;
+  if (op === "/") return a / b;
+  else return "Invalid Operator";
+}
+
+module.exports = calculator;
