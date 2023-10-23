660f9f8e1cebdc14335641e471044921db2e228c
Alex Liebhaber <alexliebhaber1992@gmail.com>
2023-10-23 09:38:03 -0500

    arr intersection done

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,14 @@
-function arrayIntersection() {}
+function arrayIntersection(arr1, arr2) {
+  const set1 = new Set(arr1);
+  const same = [];
+
+  for (const num of arr2) {
+    if (set1.has(num)) {
+      same.push(num);
+    }
+  }
+
+  return same;
+}
 
 module.exports = arrayIntersection;
