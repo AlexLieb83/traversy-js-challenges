5682b14b75b1a48807f256af340d62e56cbce3ba
Alex Liebhaber <alexliebhaber1992@gmail.com>
2023-10-23 13:59:41 -0500

    display likes done

--- 02-basic-challenges-2/03-display-likes/display-likes.js
+++ 02-basic-challenges-2/03-display-likes/display-likes.js
@@ -1,3 +1,21 @@
-function displayLikes() {}
+function displayLikes(likeArr) {
+  let message = "";
+
+  if (likeArr.length >= 4) {
+    message = `${likeArr[0]}, ${likeArr[1]} and ${
+      likeArr.length - 2
+    } others like this`;
+  } else if (likeArr.length === 3) {
+    message = `${likeArr[0]}, ${likeArr[1]} and ${likeArr[2]} like this`;
+  } else if (likeArr.length === 2) {
+    message = `${likeArr[0]} and ${likeArr[1]} like this`;
+  } else if (likeArr.length === 1) {
+    message = `${likeArr[0]} likes this`;
+  } else {
+    message = `no one likes this`;
+  }
+
+  return message;
+}
 
 module.exports = displayLikes;
