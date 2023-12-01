96f48be03d2e13aff4ba2b511ce28aa9f969bac0
Alex Liebhaber <alexliebhaber1992@gmail.com>
2023-11-30 21:04:51 -0600

    find missing number

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,15 @@
-function findMissingNumber() {}
+function findMissingNumber(arr) {
+  //if empty return 1
+  if (arr.length === 0) return 1;
+
+  //n = number of numbers in array
+  const n = arr.length + 1;
+  //gives us sum of consecutive numbers
+  const expectedSum = (n * (n + 1)) / 2;
+  //sum of numbers in array
+  const actualSum = arr.reduce((acc, c) => acc + c, 0);
+
+  return expectedSum - actualSum;
+}
 
 module.exports = findMissingNumber;
